fc584fd25c5e9633aaaa37f783cf01c51d81879b
leoFagundes <leofagundes2015@gmail.com>
2022-09-15 20:44:39 -0300

    teste

--- teste.py
+++ teste.py
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
-print("Quem ler me deve um burgão")
-=======
-<<<<<<< HEAD
+if x > 555555:
+    print("Quem ler me deve um burgão poha")
+
+
 x = 1
 cond = x > 0
 print("x é maior que 0" if cond else "x é menor que 0")
-=======
->>>>>>> e4483b0a7e71686859a8a1f741d2144e167c90dd
->>>>>>> 6b791b4cf0ac70ab1ba9617b17512418d8bc9f97
+
